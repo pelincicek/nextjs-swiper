7455e841c3382099266131df2a591310cd646204
pelincicek <pelin@ketencek.com>
2023-10-10 11:09:58 +0300

    example

--- app/components/SlideNavButtons.tsx
+++ app/components/SlideNavButtons.tsx
@@ -4,15 +4,15 @@ const SlideNavButtons = () => {
   const swiper = useSwiper();
 
   return (
-    <div className="border-2 border-gray-200 w-fit px-2 py-1 rounded-xl mt-4 space-x-4">
+    <div className=" w-fit px-2 py-1 rounded-xl mt-4 space-x-4">
       <button
-        className="bg-blue-300 p-1 rounded-md"
+        className="bg-blue-200 p-1 rounded-md bg-center"
         onClick={() => swiper.slidePrev()}
       >
         Prev
       </button>
       <button
-        className="bg-blue-300 p-1 rounded-md"
+        className="bg-blue-200 p-1 rounded-md bg-center"
         onClick={() => swiper.slideNext()}
       >
         Next
